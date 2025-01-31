a0fe77e9b7af4af453b6e2b05f3ed0b9ce3773e6
seungmo <93jsm@naver.com>
2025-01-31 19:32:00 +0900

    250131 first commit

---
+++ python1/python1_2/game/__init__.py
@@ -0,0 +1,9 @@
+from .graphic.render import render_test # .은 현재 디렉터리라는 뜻
+
+VERSION = 3.5
+
+def print_version_info():
+    print(f"The version of this game is {VERSION}")
+
+# 여기에 패키지 초기화 코드를 작성한다.
+print("Initializing game...")
---
+++ python1/python1_2/game/graphic/render.py
@@ -0,0 +1,8 @@
+from ..sound.echo import echo_test
+# from game.sound.echo import echo_test
+
+def render_test():
+    print("render")
+    echo_test()
+
+# render.py 파일의 현재 디렉터리는 graphic, 부모 디렉터리는 game이다.
